43dfa518a26cbf621163c6e6e980357cbf9e5b3e
Marleintje <marleinrusch@gmail.com>
2020-02-09 17:12:51 +0100

    format initialstate syntax

--- src/reducers/dataFetchReducer.js
+++ src/reducers/dataFetchReducer.js
@@ -1,71 +1,83 @@
-import { ADD_FETCHED_CONTENT_PER_CATEGORY, INCREMENT} from "../actions/types";
+import { ADD_FETCHED_CONTENT_PER_CATEGORY, INCREMENT } from "../actions/types";
 
 const initialState = {
-    menuItems: {
-        nl: [
-
-            {
-                category: 'Over ons',
-                subcategories: ['Verenigingsgeschiedenis', 'Ateliergebruik', 'Archief']
-            },
-            {
-                category: 'Lessen en activiteiten',
-                subcategories: ['Lessen', 'Activiteiten', 'Groepen', 'Artistieke begeleiding']
-            },
-            {
-                category: 'Prikbord',
-                subcategories: ['Nieuwtjes', 'Musea']
-            },
-            {
-                category: 'Lidmaatschap',
-                subcategories: ['Kosten', 'Informatie', 'Aanmelden als lid']
-            },
-            {
-                category: 'Contact',
-                subcategories: []
-            }   
-        ],
-        en: [
-
-            {
-                category: 'About us',
-                subcategories: ['Our history', 'Use of workshop', 'Archive']
-            },
-            {
-                category: 'Classes and activities',
-                subcategories: ['Classes', 'Activities', 'Groups', 'Artistic guidance']
-            },
-            {
-                category: 'Message board',
-                subcategories: ['Announcements', 'Museums']
-            },
-            {
-                category: 'Membership',
-                subcategories: ['Costs', 'Information', 'Apply for membership']
-            },
-            {
-                category: 'Contact',
-                subcategories: []
-            }   
-        ]
-    },
-    counter: 1,
-    language: 'nl'
+	menuItems: {
+		nl: [
+			{
+				category: "Over ons",
+				subcategories: [
+					"Verenigingsgeschiedenis",
+					"Ateliergebruik",
+					"Archief"
+				]
+			},
+			{
+				category: "Lessen en activiteiten",
+				subcategories: [
+					"Lessen",
+					"Activiteiten",
+					"Groepen",
+					"Artistieke begeleiding"
+				]
+			},
+			{
+				category: "Prikbord",
+				subcategories: ["Nieuwtjes", "Musea"]
+			},
+			{
+				category: "Lidmaatschap",
+				subcategories: ["Kosten", "Informatie", "Aanmelden als lid"]
+			},
+			{
+				category: "Contact",
+				subcategories: []
+			}
+		],
+		en: [
+			{
+				category: "About us",
+				subcategories: ["Our history", "Use of workshop", "Archive"]
+			},
+			{
+				category: "Classes and activities",
+				subcategories: [
+					"Classes",
+					"Activities",
+					"Groups",
+					"Artistic guidance"
+				]
+			},
+			{
+				category: "Message board",
+				subcategories: ["Announcements", "Museums"]
+			},
+			{
+				category: "Membership",
+				subcategories: ["Costs", "Information", "Apply for membership"]
+			},
+			{
+				category: "Contact",
+				subcategories: []
+			}
+		]
+	},
+	counter: 1,
+	language: "nl"
 };
 
-export default function (state = initialState, action) {
-    switch (action.type) {
-        case INCREMENT:
-            return {
+export default function(state = initialState, action) {
+	switch (action.type) {
+		case INCREMENT:
+			return {
 				...state,
 				counter: state.counter + 1
 			};
-        case ADD_FETCHED_CONTENT_PER_CATEGORY:
-            return {
+		case ADD_FETCHED_CONTENT_PER_CATEGORY:
+			return {
 				...state,
-				[action.category] : action.payload
+				[action.category]: action.payload
 			};
-        default:
-            return state;
-    }
+		default:
+			return state;
+	}
 }
