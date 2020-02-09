ce9fce131871dbffe6bf2cefc832e5aa5dde18fb
Marleintje <marleinrusch@gmail.com>
2020-02-09 17:26:28 +0100

    Make loop over menuItems (from Redux store)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
 import React from "react";
 import Link from "next/link";
 import { connect } from "react-redux";
-
+import slugify from "./../helpers/slugify";
 
 const Header = props => {
+	const { language, menuItems } = props;
+	console.log(menuItems.en[1].category)
+
 	return (
 		<div className="p-3">
-			<Link href="/">
-				<a>Home</a>
-			</Link>
-			<Link href="/about">
-				<a>Over ons</a>
-			</Link>
+			{menuItems[`${language}`].map( menuCategory => (
+				<Link href={slugify(menuCategory.category)}>
+					{menuCategory.category}
+				</Link>
+			))}
 		</div>
 	);
 };
 
 const mapStateToProps = state => ({
-    menuItems: state.dataFetchReducer.menuItems,
-    language: state.dataFetchReducer.language
+	menuItems: state.dataFetchReducer.menuItems,
+	language: state.dataFetchReducer.language
 });
 
 export default connect(mapStateToProps)(Header);
